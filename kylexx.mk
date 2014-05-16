4f5de8ad849bf80753ce2a57608129af00f4ef89
speedbot <ajay.katwe@gmail.com>
2014-05-16 11:51:37 +0200

    Update kylexx.mk

--- kylexx.mk
+++ kylexx.mk
@@ -2,7 +2,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-$(call inherit-product, vendor/samsung/kylexx/blobs.mk)
+$(call inherit-product, vendor/samsung/kylexx/kylexx-vendor-blobs.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/kylexx/overlay
 
